7bafdda2ba7f267df40496098332ed2fa6942f79
lxahumadau <lauraahumada571@gmail.com>
2022-03-08 16:35:32 -0500

    commit

---
+++ src/main/java/com/example/activity1/Controller/ProductsController.java
@@ -0,0 +1,28 @@
+package com.example.activity1.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductsController {
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    public String getProduct(){
+        return "Este es el método para consultar todos los productos";
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public String createProduct(){
+        return "Este es un método para crear un producto";
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.PUT)
+    public String updateProduct(){
+        return "Este es un método para actualizar un producto";
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.DELETE)
+    public String deleteProduct(){
+        return "Este es un método para eliminar un producto";
+    }
+}
